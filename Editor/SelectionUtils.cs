7af2d0ec6364b0cf3cfd528fb918744bcd9407cd
baba_s <shotababa1206@gmail.com>
2020-09-28 19:36:49 +0900

    first commit

---
+++ Editor/SelectionUtils.cs
@@ -0,0 +1,29 @@
+﻿using JetBrains.Annotations;
+using System.Linq;
+using UnityEditor;
+
+namespace Kogane
+{
+	/// <summary>
+	/// Selection 関連の汎用的な関数を管理するクラス
+	/// </summary>
+	public static class SelectionUtils
+	{
+		//================================================================================
+		// 関数(static)
+		//================================================================================
+		/// <summary>
+		/// 選択中のフォルダとそれに含まれるすべてのサブフォルダのパスを返します
+		/// </summary>
+		[NotNull]
+		public static string[] GetSelectedFolderPaths()
+		{
+			return Selection
+					.GetFiltered<DefaultAsset>( SelectionMode.DeepAssets )
+					.Select( x => AssetDatabase.GetAssetPath( x ) )
+					.Where( x => AssetDatabase.IsValidFolder( x ) )
+					.ToArray()
+				;
+		}
+	}
+}
